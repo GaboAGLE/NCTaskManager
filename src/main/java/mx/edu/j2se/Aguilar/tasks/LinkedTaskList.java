22351b6fa4a77aa030b2cae7191dbf80e2072ab4
Gabriel <gabpatmau@gmail.com>
2021-03-03 19:34:54 -0600

    practica_3_incompleta

---
+++ src/main/java/mx/edu/j2se/Aguilar/tasks/LinkedTaskList.java
@@ -0,0 +1,19 @@
+package mx.edu.j2se.Aguilar.tasks;
+
+public class LinkedTaskList {
+    void add (Task task){
+
+    }
+
+    public boolean remove (Task task){
+
+    }
+
+    public int size(){
+
+    }
+
+    public Task getTask(int index) {
+
+    }
+}
